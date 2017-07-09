0d2d647c84b71d192da5dcc7cab5094a03cf5b11
Dimitar Venov <dimitarvenov@hotmail.com>
2017-07-09 14:48:26 +0200

    Added configuration to include static files when running with Tornado server. Only thing that is needed is to run python manage.py collectstatic.

--- eambulance/urls.py
+++ eambulance/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url, include, static
+from django.conf import settings
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^comments/', include('comments.urls'), name='comments'),
     url(r'^diagnoses/', include('diagnoses.urls'), name='diagnoses')
-]
+] + static.static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
